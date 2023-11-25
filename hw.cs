491d4322cf2c7e5a367ef34729311522dd91b570
IaNeGei <152006262+IaNeGei@users.noreply.github.com>
2023-11-25 17:08:12 +0500

    Add files via upload

---
+++ hw.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello world");
+       //и зачем мы это делаем...
+            
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
